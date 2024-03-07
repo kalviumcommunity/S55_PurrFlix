04aaadb88e26e9e18fbc9084d29808f3da8eb00a
shhhreya645 <shreya.pawar@kalvium.community>
2024-03-07 11:19:06 +0530

    connection status

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
-const port = process.env.PUBLIC_PORT || 3000;
+const PORT = 3000;
+let status = "disconnected";
+dotenv.config();
 
-// define the ping route
-app.get('/ping', (req, res)=>{
-    res.send('RESPONSE');
+const startConnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        status = "connected"; 
+        console.log("Connected to MongoDB");
+    } catch (err) {
+    console.error("Error connecting to MongoDB");
+    status = "error";
+    }
+};
+
+const stopConnect = async () => {
+    await mongoose.disconnect();
+    status = "disconnected"; 
+    console.log("Disconnected from MongoDB");
+};
+
+app.get('/', (req, res) => {
+    res.send(status);
 });
 
-if (require.main === module) {
-    app.listen(port, () => {
-        console.log(`🚀 server running on PORT: ${port}`);
-    });
-}
+app.listen(PORT, async () => {
+    await startConnect(); 
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
