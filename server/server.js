c3ff80f742f0812cb1aa708c1555c08f1377ee65
shhhreya645 <shreya.pawar@kalvium.community>
2024-03-11 14:42:04 +0530

    added routes

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const routes = require('./routes'); 
+const app = express();
+const PORT = 3000;
+let status = "disconnected";
+
+
+
+dotenv.config();
+
+const startConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    status = "connected"; 
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:");
+    status = "error";
+  }
+};
+
+const stopConnect = async () => {
+  await mongoose.disconnect();
+  status = "disconnected"; 
+  console.log("Disconnected from MongoDB");
+};
+
+app.use('/', routes);
+
+app.get('/', (req, res) => {
+  res.send(status);
+});
+
+app.listen(PORT, async () => {
+  await startConnect(); 
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
