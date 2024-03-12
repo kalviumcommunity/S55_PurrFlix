00a4317ab575fe9c9b274eda0ddd247ebd915881
shhhreya645 <shreya.pawar@kalvium.community>
2024-03-12 17:49:24 +0530

    added basic landing page

---
+++ client/Purrflix/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="navbar">
+                    {/* navbar */}
+                </div>
+
+                <div className="content">
+                    <h1>PURRFLIX</h1>
+                    <input className="search" type="text" placeholder="Search..." />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
