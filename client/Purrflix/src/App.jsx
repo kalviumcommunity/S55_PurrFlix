43e9c521891585104c3600bac1fdfeb71ef9ee37
shhhreya645 <shreya.pawar@kalvium.community>
2024-04-10 14:24:26 +0530

    login logout

--- client/Purrflix/src/App.jsx
+++ client/Purrflix/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Form from './components/Form';
 import UpdateEntityForm from './components/UpdateEntityForm';
+import Login from './components/Login';
+import Logout from './components/Logout'; 
 import './App.css';
 
 const App = () => {
@@ -9,7 +11,9 @@ const App = () => {
         <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/add-entity" element={<Form />} />
-            <Route path="/update-entity/:id" element={<UpdateEntityForm />} /> 
+            <Route path="/update-entity/:id" element={<UpdateEntityForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} /> 
         </Routes>
     );
 }
---
+++ client/Purrflix/src/components/Login.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const navigate = useNavigate();
+    const [credentials, setCredentials] = useState({ username: '', password: '' });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials({
+            ...credentials,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('/login', credentials);
+            navigate('/'); 
+        } catch (error) {
+            console.error('Error logging in:', error);
+            window.alert('Error logging in. Please try again.');
+        }
+    };
+
+    return (
+        <div className='form-container'>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={credentials.username}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={credentials.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
