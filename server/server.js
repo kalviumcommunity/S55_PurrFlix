cc3522589413e4db71e42dc3d0e585b18474fb1e
shhhreya645 <shreya.pawar@kalvium.community>
2024-04-11 23:20:13 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const { router } = require('./routes');
+const router = require('./routes'); 
 const cookieParser = require('cookie-parser'); 
 const app = express();
 const cors = require('cors');
